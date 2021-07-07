b0e7ccc69853fa84e5cc281e45fa86d000cab774
Rémi Launay <remi@whisperies.com>
2021-07-07 14:11:28 +0200

    logment list / item comp

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import LodgmentList from '../components/LodgmentList/LodgmentList'
+
+import api from './../mocks/api'
+
+
 class Home extends React.Component {
+    state = {
+        lodgments: [],
+        error: null
+    }
+
+    componentDidMount() {
+        api.getAll().then(res => {
+            this.setState({ lodgments: res, error: null })
+        }).catch(err => {
+            this.setState({ error: err })
+        })
+    }
+
     render() {
         return (
             <div>
                 <h1>Home</h1>
+                <LodgmentList lodgments={this.state.lodgments} />
             </div>
         )
     }
