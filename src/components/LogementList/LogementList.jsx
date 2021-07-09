812741037fe051edc13ace7dfdd19982ee5db34b
Remi Launay <rlaunay25@gmail.com>
2021-07-09 19:23:18 +0200

    add loader / error

--- src/components/LogementList/LogementList.jsx
+++ src/components/LogementList/LogementList.jsx
@@ -6,11 +6,21 @@ import classes from './LogementList.module.scss'
 
 class LogementList extends React.Component {
     render() {
+        let logements = this.props.lodgments.map(lod => (
+            <LogementItem key={lod.id} id={lod.id} title={lod.title} cover={lod.cover}/>
+        ))
+
+        if (logements.length === 0) {
+            logements = <h3>Aucun logements trouvé !</h3>
+        }
+
+        if (this.props.error) {
+            logements = <h3>Une erreur est survenue</h3>
+        }
+
         return (
             <section className={classes.grid}>
-                {this.props.lodgments.map(lod => (
-                    <LogementItem key={lod.id} id={lod.id} title={lod.title} cover={lod.cover}/>
-                ))}
+                {logements}
             </section>
         )
     }
