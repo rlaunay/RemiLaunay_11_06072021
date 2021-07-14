1af49ab0dc0215701d2698b176967f24fa9f4158
Rémi Launay <relaunay@gmail.com>
2021-07-15 01:00:06 +0200

    not found page + redirects

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
+import H1 from '../components/UI/Title/H1/H1'
+import H2 from '../components/UI/Title/H2/H2'
+import StyledLink from './../components/UI/StyledLink/StyledLink'
+
 class NotFound extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Not Found</h1>
-            </div>
+            <React.Fragment>
+                <H1>404</H1>
+                <H2>Oups! La page que vous demandez n'existe pas.</H2>
+                <StyledLink to="/" >Retourner sur la page d’accueil</StyledLink>
+            </React.Fragment>
         )
     }
 }
